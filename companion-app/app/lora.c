9f0fd0317a8b5a2c37943c8847d26ccd29b30f37
nber08 <nagyberci08@gmail.com>
2026-03-23 15:21:09 +0100

    Continued companion app

--- companion-app/app/lora.c
+++ companion-app/app/lora.c
@@ -6,23 +6,24 @@ static struct sp_port* port = NULL;
 static int8_t last_rssi = 0, last_snr = 0;
 
 void lora_init(const char* port, uint32_t baud, const LoraConfig_t* cfg) {
-    int fd = open(port, O_RDWR | O_NOCTTY);
-	if (fd < 0) { perror("open"); return 1; }
+    // int fd = open(port, O_RDWR | O_NOCTTY);
+	// if (fd < 0) { perror("open"); return 1; }
 
-    // Configure it
-	struct termios tty;
-	tcgetattr(fd, &tty);
-	cfsetspeed(&tty, baud);
-	tty.c_cflag = CS8 | CREAD | CLOCAL;  // 8N1, no flow control
-	tty.c_iflag = 0;
-	tty.c_oflag = 0;
-	tty.c_lflag = 0;
-	tty.c_cc[VMIN]  = 0;   // non-blocking read
-	tty.c_cc[VTIME] = 10;  // 1 second read timeout (in tenths of a second)
-	tcsetattr(fd, TCSANOW, &tty);
+    // // Configure it
+	// struct termios tty;
+	// tcgetattr(fd, &tty);
+	// cfsetspeed(&tty, baud);
+	// tty.c_cflag = CS8 | CREAD | CLOCAL;  // 8N1, no flow control
+	// tty.c_iflag = 0;
+	// tty.c_oflag = 0;
+	// tty.c_lflag = 0;
+	// tty.c_cc[VMIN]  = 0;   // non-blocking read
+	// tty.c_cc[VTIME] = 10;  // 1 second read timeout (in tenths of a second)
+	// tcsetattr(fd, TCSANOW, &tty);
 
     for (i = 0; i< sizeof(cfg->payload); i++) {
         const char* msg = cfq[i];
         write(fd, msg, strlen(msg));
+        Serial.println(msg);
     }
 }
